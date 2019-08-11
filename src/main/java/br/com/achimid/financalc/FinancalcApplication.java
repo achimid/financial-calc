d406996a7f5137337311a49f3bda2e59cc24de70
Achimid <achimid@hotmail.com>
2019-08-11 16:42:22 -0300

    initial commit

---
+++ src/main/java/br/com/achimid/financalc/FinancalcApplication.java
@@ -0,0 +1,13 @@
+package br.com.achimid.financalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancalcApplication.class, args);
+	}
+
+}
