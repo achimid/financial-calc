9c644e6cca5deb8179d42ac8bd5a74ef845106a7
Achimid <achimid@hotmail.com>
2019-08-11 16:40:04 -0300

    Initial commit

---
+++ src/main/java/br/com/achimid/financalc/utils/Constants.java
@@ -0,0 +1,11 @@
+package br.com.achimid.financalc.utils;
+
+import java.math.BigDecimal;
+
+public class Constants {
+
+    public static final BigDecimal SELIC_ANUAL_PERCENT = new BigDecimal(6.4);
+    public static final BigDecimal DIIN_PAYOUT_PERCENT = new BigDecimal(100);
+    public static final BigDecimal DIIN_PROFITABLE_DAYS = new BigDecimal(252);
+
+}
